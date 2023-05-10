74132fed03f29b76bc967e089ab7648c2504514c
240763 <240763@edu.p.lodz.pl>
2023-05-11 00:40:46 +0200

    Dodano przejście między tłem głównego ekranu menu a rozmytym tłem głównego ekranu menu. Dodano również możliwość cofania się do podstawowego tła klawiszem ESC.

--- core/src/screens/MainMenu.java
+++ core/src/screens/MainMenu.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.utils.ScreenUtils;
@@ -19,6 +20,8 @@ public class MainMenu implements Screen{
 
     private Texture mainMenuBackground;
     private Texture mainMenuBackgroundBlured;
+    private Sprite mainMenuBackgroundBluredSprite;
+    private float mainMenuBackgroundBluredSpriteAlpha;
     private Texture tempTexture;
     private GameObject buttonExit;
     private GameObject buttonStart;
@@ -30,27 +33,36 @@ public class MainMenu implements Screen{
     @Override
     public void show() {
         mainMenuBackground = new Texture("Menu/TC_Menu_MainBackground1.png");
-        mainMenuBackgroundBlured = new Texture("Menu/MenuTransition1/MenuTransition1_11.png");
+        mainMenuBackgroundBlured = new Texture("Menu/TC_Menu_MainBackground1Blured.png");
         tempTexture = new Texture("tempRed.png");
         buttonExit = new GameObject(tempTexture, 1436, 331, 262, 97);
         buttonStart = new GameObject(tempTexture, 1408, 473, 330, 109);
+        mainMenuBackgroundBluredSprite = new Sprite(mainMenuBackgroundBlured);
+        mainMenuBackgroundBluredSprite.setPosition(0,0);
+        mainMenuBackgroundBluredSprite.setSize(1920,1080);
+        mainMenuBackgroundBluredSpriteAlpha = 0;
     }
 
     @Override
     public void render(float delta) {
-        ScreenUtils.clear(0, 0, 0, 1);
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //ScreenUtils.clear(0, 0, 0, 1);
+        //Gdx.gl.glClearColor(0, 0, 0, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         Vector3 cursorPosition3 = game.viewport.unproject(new Vector3(Gdx.input.getX(), Gdx.input.getY(), 0));
         Vector2 cursorPosition = new Vector2(cursorPosition3.x, cursorPosition3.y);
 
         game.batch.begin();
 
-        if(!startPressed){
-            game.batch.draw(mainMenuBackground, 0, 0,1920,1080);
+        game.batch.draw(mainMenuBackground, 0, 0,1920,1080);
+        if(startPressed){
+            if(mainMenuBackgroundBluredSpriteAlpha<0.9f) mainMenuBackgroundBluredSprite.setAlpha(mainMenuBackgroundBluredSpriteAlpha+=7f*delta);
+            else mainMenuBackgroundBluredSprite.setAlpha(1);
+            mainMenuBackgroundBluredSprite.draw(game.batch);
         }
-        else{
-            game.batch.draw(mainMenuBackgroundBlured, 0, 0,1920,1080);
+        else if(mainMenuBackgroundBluredSpriteAlpha>0){
+            if(mainMenuBackgroundBluredSpriteAlpha>0.15f) mainMenuBackgroundBluredSprite.setAlpha(mainMenuBackgroundBluredSpriteAlpha-=7f*delta);
+            else mainMenuBackgroundBluredSprite.setAlpha(mainMenuBackgroundBluredSpriteAlpha=0);
+            mainMenuBackgroundBluredSprite.draw(game.batch);
         }
 
 
@@ -59,10 +71,14 @@ public class MainMenu implements Screen{
                 startPressed = true;
             }
         }
-        if(buttonExit.contains(cursorPosition) && !startPressed){
+        else if(buttonExit.contains(cursorPosition) && !startPressed){
             if(Gdx.input.isButtonJustPressed(Input.Buttons.LEFT)){
                 Gdx.app.exit();
             }
+        }
+
+        if(startPressed && Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)){
+            startPressed = false;
         }
         game.batch.end();
     }
@@ -90,5 +106,7 @@ public class MainMenu implements Screen{
     @Override
     public void dispose() {
         mainMenuBackground.dispose();
+        mainMenuBackgroundBlured.dispose();
+        tempTexture.dispose();
     }
 }
