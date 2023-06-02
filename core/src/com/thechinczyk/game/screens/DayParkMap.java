e054eeb6f070f2da40c972bf4c48729a6b7e1a20
Eryk Skonieczny <240796@edu.p.lodz.pl>
2023-06-02 14:11:53 +0200

    drobne poprawki m.in. włączenie animacji rzutu kostką

--- core/src/com/thechinczyk/game/screens/DayParkMap.java
+++ core/src/com/thechinczyk/game/screens/DayParkMap.java
@@ -447,12 +447,14 @@ public class DayParkMap implements Screen {
     }
 
     public void setPlayerNumberTurn() {
-        if (playerNumberTurn < game.playerCount - 1) {
-            playerNumberTurn++;
-        } else {
-            playerNumberTurn = 0;
+        if(!endGame) {
+            if (playerNumberTurn < game.playerCount - 1) {
+                playerNumberTurn++;
+            } else {
+                playerNumberTurn = 0;
+            }
+            changeTurn = true;
         }
-        changeTurn = true;
     }
 
     public void drawWhichPlayersTurnUI() {
@@ -539,7 +541,7 @@ public class DayParkMap implements Screen {
             gameTextures.diceAnimStarted = true;
         }
         if (gameTextures.diceAnimStarted) {
-            /*if (!gameTextures.diceAnim.isAnimationFinished(gameTextures.diceElapsedTime)) {
+            if (!gameTextures.diceAnim.isAnimationFinished(gameTextures.diceElapsedTime)) {
                 gameTextures.diceElapsedTime += Gdx.graphics.getDeltaTime();
                 game.batch.draw(gameTextures.diceAnim.getKeyFrame(gameTextures.diceElapsedTime, false), 300, 0, 1000, 850);
             } else {
@@ -547,14 +549,14 @@ public class DayParkMap implements Screen {
                 gameTextures.diceElapsedTime = 0;
                 throwDice = true;
             }
-            if(gameTextures.diceAnim.getKeyFrameIndex(gameTextures.diceElapsedTime) == 55){*/
+            if(gameTextures.diceAnim.getKeyFrameIndex(gameTextures.diceElapsedTime) == 55){
                 randNumber = rand.nextInt(6) + 1;
                 diceRoll = randNumber;
                 //System.out.println(randNumber);
-                gameTextures.diceAnimStarted = false;
-                gameTextures.diceElapsedTime = 0;
-                throwDice = true;
-            //}
+                //gameTextures.diceAnimStarted = false;
+                //gameTextures.diceElapsedTime = 0;
+                //throwDice = true;
+            }
         }
     }
 
